65a0fff117f711c6afd9e6ee11e22a54ee764231
kiwanganewton <kiwanganewton3@gmail.com>
2026-09-14 12:52:18 +0300

    Update

--- src/components/landing/hero/hero.config.ts
+++ src/components/landing/hero/hero.config.ts
@@ -38,7 +38,7 @@ export const heroSlides: HeroSlideData[] = [
     description:
       "Running your business is already a full-time job, and you miss doing  ",
 
-    image: "/images/hero/slide-000000001.webp",
+    image: "/images/hero/slide-00000000001.webp",
 
     cta: {
       label: "Dicover More",
